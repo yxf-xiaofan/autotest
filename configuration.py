e3d9851b8cd68e6ced39459f55383b3af25e1216
yxf-xiaofan <1719262040@qq.com>
2022-06-08 15:26:34 +0800

    xuigai

--- configuration.py
+++ configuration.py
@@ -1,5 +1,17 @@
+import sys
+sys.path.append("C:\\Users\\fanhao\\Desktop\\auto\\autotest\\configuration.py")
+
+
+
+
 #配置浏览器
 driver_type = "chrome"
 
 #配置地址
-url = "http://avengers.wandianzhang.com"
+def test_url():
+    return "http://avengers.wandianzhang.com"
+
+
+
+if __name__ == 'main':
+    url = test_url()
--- page/login.py
+++ page/login.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from time import sleep
-import sys
-
-"""sys.path.append("C:\Users\fanhao\Desktop\auto\autotest\configuration.py")"""
 from configuration import *
 
 driver = webdriver.Chrome()
-driver.get(url)
+driver.get(test_url())
 sleep(3)
 
 
