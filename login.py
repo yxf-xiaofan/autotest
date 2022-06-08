ab5f491bb855d0f205dde384435bec2c1e761088
yxf-xiaofan <1719262040@qq.com>
2022-06-08 10:43:11 +0800

    删除

--- login.py
+++
@@ -1,9 +0,0 @@
-from lib2to3.pgen2 import driver
-from selenium import webdriver
-from time import sleep
-
-driver = webdriver.Chrome()
-driver.get("http://avengers.wandianzhang.com/#/login")
-sleep(3)
-
-
